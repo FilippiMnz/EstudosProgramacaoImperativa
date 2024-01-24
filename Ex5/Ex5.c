d835cb6e2b0b4b0581430f8b51b62eb590aa1eb7
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 11:07:49 -0300

    Update Ex5.c

--- Ex5/Ex5.c
+++ Ex5/Ex5.c
@@ -1,11 +1,12 @@
+// algoritmo que calcula o tempo, recebendo a entrada de 2 dados(velocidade e distancia) do cliente e retornando uma saida(tempo)
 #include <stdio.h>
 int main()
  {
- float velocidade, distancia;
+ float velocidade, distancia; // inicialização de variaveis 
  printf("Insira a distancia pretendida para o exercicio Em Metros: \n");
- scanf("%f", &distancia);
+ scanf("%f", &distancia); // guarda distancia
  printf("Insira a Velocidade esperada para a execução em Km: \n");
- scanf("%f", &velocidade);
- float tempo = distancia/velocidade;
- printf("Voce levara em media %0.2f minutos para chegar ao seu destino", tempo);
+ scanf("%f", &velocidade); // guarda velocidade
+ float tempo = distancia/velocidade; // calcula o tempo dividindo a distancia pela velocidade 
+ printf("Voce levara em media %0.2f minutos para chegar ao seu destino", tempo); // imprime a saida(tempo) para o cliente
  } 
