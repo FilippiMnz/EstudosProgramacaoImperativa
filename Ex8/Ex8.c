d1682a4e8a90e182dd25ea24cec0aa6792c206d3
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 11:13:16 -0300

    Update Ex8.c

--- Ex8/Ex8.c
+++ Ex8/Ex8.c
@@ -1,12 +1,13 @@
+// algoritmo que calcula a area de um triangulo, recebendo 2 entradas e tendo uma saida
 #include <stdio.h>
 int main()
 {
- float largura, comprimento;
+ float largura, comprimento; // inicialização de variaveis 
  printf("Digite a largura do triangulo: \n");
- scanf("%f", &largura);
+ scanf("%f", &largura); // entrada de dado1 largura
  printf("Digite o comprimento do Triangulo: \n");
- scanf("%f", &comprimento);
+ scanf("%f", &comprimento); // entrada de dado2 comprimento
  // Regra para calculo da area triangulo
- float area = comprimento * largura;
- printf("A area do retangulo é de %0.2f metros quadrados \n", area);
+ float area = comprimento * largura; // calculo da area, multiplicando comprimento pela largura
+ printf("A area do retangulo é de %0.2f metros quadrados \n", area); // saida de dados
 }
