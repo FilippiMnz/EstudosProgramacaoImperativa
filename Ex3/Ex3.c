550e57af6b9defd480829de127e76dec63484b3d
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 10:59:50 -0300

    Update Ex3.c

--- Ex3/Ex3.c
+++ Ex3/Ex3.c
@@ -1,13 +1,14 @@
+// algoritmo que calcula os anos que deverão ser tomadas doses de vacinas, de 4 doses
 #include <stdio.h>
 int main()
 {
- int Doses, Ano;
+ int Doses, Ano; // inicialização de variavel 
  printf("Insira o Ano da Primeira Dose \n");
  scanf("%d", &Ano);
  printf("Insira o periodo de tempo entre as doses: \n");
  scanf("%d", &Doses);
  printf("Eis os anos das proximas doses: \n");
-   for(int i = 1; i < 4; i++)
+   for(int i = 1; i < 4; i++) // laço de for que para todo i = 1, enquanto ir for menor a 4 ele multiplica o i pela quantidade de doses e soma com o ano atual e retorna o da proxima dose
  {
    printf("%d \n", Ano + (i * Doses));	
  }
