fcec5b0c15e25326cf45e9a0f571586dad2c4fd6
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 10:50:40 -0300

    Update Ex2.c

--- Ex2/Ex2.c
+++ Ex2/Ex2.c
@@ -1,7 +1,8 @@
+// algoritimo para Calular o Numero de Dias para alcançar o peso ideal, rebendo 3 dados do client e retornando o numero de dias
 #include <stdio.h>
 int main()
 {
- float pesoAtual, pesoIdeal, perda;
+ float pesoAtual, pesoIdeal, perda; // inicialização de variaveis vazias
  int dias = 0;
  printf("Insira o Seu peso atual em kg: \n");
  scanf("%f", &pesoAtual);
@@ -9,17 +10,17 @@ printf("Insira o seu Peso Ideal Em kg: \n");
 scanf("%f", &pesoIdeal);
 printf("digite a perda mensal em kg: \n");
 scanf("%f", &perda);
- if(pesoAtual <= pesoIdeal)
+ if(pesoAtual <= pesoIdeal) // verificação se o peso ja está no ideal ou está abaixo
  {
 	 printf("Voce ja se encontra abaixo ou exatamente no peso ideal. \n");
  } else
  {
- while(pesoAtual > pesoIdeal) 
+ while(pesoAtual > pesoIdeal)  // caso esteja acima do ideal, executará um loop, Enquanto(while) o peso atual for maior que o ideal
    {
    pesoAtual -= perda;
    dias++;
    }
-   printf("Serão necessarios %d dias para alcançar o peso ideal. \n", dias);
+   printf("Serão necessarios %d dias para alcançar o peso ideal. \n", dias); // retorna a quantidade de Dias 
  }
  return 0;
 }
