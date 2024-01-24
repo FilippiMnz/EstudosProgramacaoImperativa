2f5d9c5172de463932f753dde26137bcff369517
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 11:11:50 -0300

    Update Ex7.c

--- Ex7/Ex7.c
+++ Ex7/Ex7.c
@@ -1,10 +1,11 @@
+// algoritmo que calcula a conversão de celcius para fahrenheit, rebendo uma entrada e tendo uma saida
 #include <stdio.h>
 int main()
 {
-  float celcius;
+  float celcius; //inicialização de variavel
   printf("Insira a temperatura em Celcius para a conversao: \n");
-  scanf("%f", &celcius);
+  scanf("%f", &celcius); // entrada de dado
   //regra de conversão (celcius × 9/5) + 32
-  float fahrenheit = (celcius * 9/5) +32;
-  printf("Sua temperatura convertida em Fahrenheit é %0.2f \n", fahrenheit);
+  float fahrenheit = (celcius * 9/5) +32; // calculo de conversão
+  printf("Sua temperatura convertida em Fahrenheit é %0.2f \n", fahrenheit); // imprime a saida 
 }
