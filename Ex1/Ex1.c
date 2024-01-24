198e02223d0660b097468baaeed9689545e4028a
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 10:47:11 -0300

    Update Ex1.c

--- Ex1/Ex1.c
+++ Ex1/Ex1.c
@@ -1,11 +1,12 @@
+// algoritimo que recebe um valor qualquer e retorna seu sucessor e antecessor
 #include <stdio.h>
 int main()
 {
-  int NumberClient;
-  printf("Digite Um numero Qualquer: \n");
-  scanf("%d", &NumberClient);
-  int NumberDecre = NumberClient - 1;
-  int NumberIncre = NumberClient + 1;
-  printf("O suvessor e antessesor do seu numero sao respectivamente: %d %d", NumberIncre, NumberDecre);
+  int NumberClient; // inicialização da variavel que receberá o dado do Client
+  printf("Digite Um numero Qualquer: \n"); //recebe o dado 
+  scanf("%d", &NumberClient); // armazena o dado
+  int NumberDecre = NumberClient - 1; // Decrementa o Dado em Uma unidade para retornar seu antecessor
+  int NumberIncre = NumberClient + 1; // Incremente o dado em Uma unidade para retornar seu Sucessor
+  printf("O suvessor e antessesor do seu numero sao respectivamente: %d %d", NumberIncre, NumberDecre); // Imprime os valores para o Client
   return 0;
 }
