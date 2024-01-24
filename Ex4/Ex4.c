4b2da05073b1af1e51ffd0e9e3e2c9d33dec1b3b
Filippi Reis <85193619+FilippiMnz@users.noreply.github.com>
2024-01-24 11:01:56 -0300

    Update Ex4.c

--- Ex4/Ex4.c
+++ Ex4/Ex4.c
@@ -1,11 +1,12 @@
+// algoritimo que faz o cambio de reais para dolar 
 #include <stdio.h>
 int main()
 {
-float real;
+float real; // inicialização de variavel para receber o real
 printf("Insira A quantidade de Reais para o cambio: \n");
-scanf("%f", &real);
-float doleta = 0.2;
-float convert = doleta*real;
-printf("Voce possui um Total de %0.2f doletas", convert);
+scanf("%f", &real); // guarda o dado da moeda do cliente
+float doleta = 0.2; // taxa de cambio dolar
+float convert = doleta*real; // calculo de conversão
+printf("Voce possui um Total de %0.2f doletas", convert); // imprime o resultado para o cliente
 return 0;
 }
